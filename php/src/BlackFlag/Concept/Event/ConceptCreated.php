5b116ec1c990c1e24ad91d820e4ac73cb4d034f7
Ludovic FLEURY <ludovic.fleury@golem.ai>
2020-11-15 15:53:07 +0100

    Introduce Black Flag character concept

---
+++ php/src/BlackFlag/Concept/Event/ConceptCreated.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BlackFlag\Concept\Event;
+
+use EventSourcing\Event;
+
+class ConceptCreated implements Event
+{
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function toArray(): array
+    {
+        return  [
+            'name' => $this->name,
+        ];
+    }
+
+    static public function fromArray(array $data): self
+    {
+        return new self(
+            $data['name'],
+        );
+    }
+
+}
---
+++ php/src/BlackFlag/Concept/Event/SetBackground.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BlackFlag\Concept\Event;
+
+use EventSourcing\Event;
+
+class SetBackground implements Event
+{
+    private string $background;
+
+    public function __construct(string $background)
+    {
+        $this->background = $background;
+    }
+
+    public function getBackground(): string
+    {
+        return $this->background;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'background' => $this->background,
+        ];
+    }
+
+    static public function fromArray(array $data): self
+    {
+        return new self(
+            $data['background'],
+        );
+    }
+
+}
